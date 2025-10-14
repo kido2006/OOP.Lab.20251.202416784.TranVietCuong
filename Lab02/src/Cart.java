1d46bfea6f156268b1a4388288d301d6c0e4b440
kido2006 <cuong.tv2416784@sis.hust.edu.vn>
2025-10-15 00:09:41 +0700

    Add Lab02

---
+++ Lab02/src/Cart.java
@@ -0,0 +1,39 @@
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED=20;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+	private int quantityOrdered = 0;
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if(quantityOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOrdered[quantityOrdered] = disc;
+			quantityOrdered += 1;
+			System.out.println("The disc has been added.");
+		}
+		else {
+			System.out.println("The cart is almost full.");
+		}
+	}
+	
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		for(int i=0; i< quantityOrdered; i++) {
+			if(itemsOrdered[i] == disc) {
+				for(int j=i; j < quantityOrdered-1; j++) {
+					itemsOrdered[j] = itemsOrdered[j+1];
+				}
+				quantityOrdered -=1;
+				System.out.println("The disc has been removed.");
+				return;
+			}
+		}
+		System.out.println("Disc not found.");
+	}
+	
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < quantityOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
