a2b1b35d657d4610b9fb73a9ab76fff232ce79dd
kido2006 <cuong.tv2416784@sis.hust.edu.vn>
2025-10-29 22:14:12 +0700

    Add printCart

---
+++ Lab02/src/CartTest.java
@@ -0,0 +1,31 @@
+
+public class CartTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin", "Animation", "John Musker", 90, 18.99f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+
+        cart.printCart();
+
+        System.out.println("Search by ID:");
+        cart.searchById(2);
+
+        System.out.println("Search by ID:");
+        cart.searchById(5);
+        
+        System.out.println("Search by Title:");
+        cart.searchByTitle("Aladdin");
+        
+        System.out.println("Search by Title:");
+        cart.searchByTitle("Doraemon");
+	}
+
+}
