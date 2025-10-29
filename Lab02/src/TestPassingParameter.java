3e39d96aed03d6f0444c443a0c1e5f1246fd6721
kido2006 <cuong.tv2416784@sis.hust.edu.vn>
2025-10-29 16:50:25 +0700

    Add Testing Passing Parameter

--- Lab02/src/TestPassingParameter.java
+++ Lab02/src/TestPassingParameter.java
@@ -11,20 +11,30 @@ public class TestPassingParameter {
         System.out.println("jungle dvd title: " + jungleDVD.getTitle());
         System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
-        changeTitle(jungleDVD, cinderellaDVD.getTitle());
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+//        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+//        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
     }
 
-    public static void swap(Object o1, Object o2) {
-    	Object tmp = o1;
-        o1 = o2;
-        o2 = tmp;
+	
+//    public static void swap(Object o1, Object o2) {
+//    	Object tmp = o1;
+//        o1 = o2;
+//        o2 = tmp;
+//    }
+//    
+//    
+//    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+//    	String oldTitle = dvd.getTitle();
+//    	dvd.setTitle(title);
+//    	dvd = new DigitalVideoDisc(oldTitle);
+//    }
+    
+	// swap() method that can correctly swap the two objects
+    public static void swap(DigitalVideoDisc a, DigitalVideoDisc b) {
+        String t = a.getTitle();
+        a.setTitle(b.getTitle());
+        b.setTitle(t);
     }
 
-    public static void changeTitle(DigitalVideoDisc dvd, String title) {
-    	String oldTitle = dvd.getTitle();
-    	dvd.setTitle(title);
-    	dvd = new DigitalVideoDisc(oldTitle);
-    }
 
 }
